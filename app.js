7b6773cd39fdadaedaf3c045b858140aa95b2a4d
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-09 17:59:58 -0700

    adding in more projects

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get("/museum", function(req, res){
   res.render("museum");
 });
 
+app.get("/patatap", function(req, res){
+  res.render("patatap");
+});
+
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
   console.log("server started");
 });
