d7d150115d8acd92aaba359682506ceceb40ca43
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-26 10:07:20 -0700

    starting new website project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function (req, res) {
+    res.render("home.ejs");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server has started");
+});
+
+
